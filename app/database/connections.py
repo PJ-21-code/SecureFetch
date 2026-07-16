ac989688f009d2e9ec89f388e2f86c0d8cdf0a80
PJ-21-code <pr.pranjal21@gmail.com>
2026-07-16 16:43:08 +0530

    database engine created

---
+++ app/database/connections.py
@@ -0,0 +1,57 @@
+import sqlite3
+import os
+
+DB_NAME= "sap_data.db"
+
+def init_db():
+
+    if not os.path.exists(DB_NAME):
+        conn= sqlite3.connect(DB_NAME)
+        cursor= conn.cursor()
+
+        cursor.execute("""
+        CREATE TABLE Transactions(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            request_id TEXT NOT NULL,
+            nid TEXT,
+            nlineNo TEXT,
+            docNo TEXT,
+            itemNumber TEXT,
+            transactionDate TEXT,
+            polholdName TEXT,
+            dC TEXT,
+            accntCode TEXT,
+            amount REAL,
+            product TEXT,
+            channel TEXT,
+            location TEXT,
+            assignment TEXT,
+            transactionReference TEXT,
+            chequeNo TEXT,
+            sourceSystem TEXT,
+            strPolCostCtr TEXT,
+            strUtrNbr TEXT,
+            strAgentCd TEXT,
+            strPvNum TEXT,
+            strDtlUinCode TEXT,
+            sapBatchNo TEXT,
+            sapRecord TEXT,
+            sapRecordDt TEXT,
+            dtCreated TEXT,
+            strCreatedBy TEXT,
+            transactionDateEod TEXT           
+            );
+
+        """)
+
+        conn.commit()
+        conn.close()
+        print(f"Database {DB_NAME} created succesfully")
+
+
+def get_db_connection():
+
+    init_db()
+    conn = sqlite3.connect(DB_NAME)
+    conn.row_factory = sqlite3.Row
+    return conn    
--- app/service/db_service.py
+++ app/service/db_service.py
@@ -1,5 +1,25 @@
 from app.schemas.transactions import TransactionItem
-
+from app.database.connections import get_db_connection
 async def get_items_from_db(request_id: str):
+    conn = get_db_connection()
+    try:
+      cursor = conn.cursor()
 
-    pass
+      cursor.execute("SELECT * FROM Transaction WHERE request_id= ?", (request_id,))
+
+      rows= cursor.fetchall()
+
+      items= [dict(row) for row in rows]
+
+      validate_items=[]
+      for item in items:
+          validate_item= TransactionItem(**item)
+          validate_items.append(validate_item.model_dump())
+
+      return validate_items
+
+    except Exception as e:
+       print(f"Database Error: {str(e)}")
+       raise e
+
+    conn.close()    
